3b269afe124e401c8cca5490eb3735b09c86fa80
fequierefaro <florence@is2ifa-15.univ-lyon1.fr>
2015-03-05 17:31:06 +0100

    Faro et Shuai : Authentification (appel au serveur)

---
+++ Biblio/Views/Authent.h
@@ -0,0 +1,16 @@
+//
+//  Authent.h
+//  Biblio
+//
+//  Created by florence martin on 05/03/2015.
+//  Copyright (c) 2015 ISFA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Authent : NSObject
+
+@property(nonatomic,strong) NSString *login;
+@property(nonatomic,strong) NSString *pass;
+
+@end
